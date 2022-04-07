619b0a0b299bf7333f4f84c1fef6610293947e99
Leah1484 <leahnjoki1484@gmail.com>
2022-04-07 02:24:07 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
-{ 
 #include <stdio.h>
-int main(void) 
+/**
+ *main - entry point
+ *
+ *Return: return 2 after program execution
+ */
+int main(void)
 {
 write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
 return (1);
 }
-}
+
